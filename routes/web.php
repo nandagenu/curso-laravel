4f7a3cd6ec347e3e796f27a2f562407bc208f29e
FERNANDA DE FÁTIMA MOREIRA GENÚ <fernanda.genu@ambiental.local>
2024-09-06 17:00:42 -0300

    rotas any, match e parametros

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,27 @@ Route::get('/', function () {
 Route::get('/empresa', function() {
     return view('site/empresa');
 });
+
+Route::any('/any', function() {
+    return "Permite todo tipo de acesso http (put, delete, get, post)";
+});
+
+Route::match(['get', 'post'], '/match', function() {
+    return "Permite apenas acessos definidos";
+});
+
+Route::get('/produto/{id}/{cat?}', function($id, $cat = '') {
+    return "O id do produto é:".$id."<br>"."A categoria é:".$cat;
+});
+
+Route::get('/sobre', function() {
+	return redirect('/empresa');
+	});
+
+Route::get('/news', function() {
+    return view('news');
+})->name('noticias');
+
+Route::get('/novidades', function (){
+    return redirect()->route('noticias');
+});
