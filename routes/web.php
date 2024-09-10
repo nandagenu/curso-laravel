57205f80886faa0d28924dec23a988e5fc2efcb2
FERNANDA DE FÁTIMA MOREIRA GENÚ <fernanda.genu@ambiental.local>
2024-09-10 15:25:18 -0300

    resources

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 
-Route::get('/', [ProdutoController::class, 'index'])->name('produto.index');
-
-Route::get('/produto/{id?}', [ProdutoController::class, 'show'])->name('produto.show');
+Route::resource('produtos', ProdutoController::class);
