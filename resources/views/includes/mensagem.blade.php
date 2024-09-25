48d6568cf4ec66e8c22fe6c96477ceeec04e6a32
nandagenu <nandafmgenu@gmail.com>
2024-09-25 16:52:00 -0300

    include e component

---
+++ resources/views/includes/mensagem.blade.php
@@ -0,0 +1,2 @@
+<h1> {{ $titulo }} </h1>
+<p> Um texto qualquer </p>
--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -2,22 +2,12 @@
 @section('title', 'Essa é a página HOME')
 @section('conteudo')
 
-{{--Estruturas de Repetição--}}
+@include('includes.mensagem', ['titulo' => 'Mensagem de sucesso!'])
 
-@for ($i = 0; $i < $count; $i++)
-    valor atual é {{ $i }} <br>
-@endfor
-
-@php $i = 0;
-@endphp
-
-@while ($i <= 15)
-    valor atual é {{ $i }} <br>
-    @php $i++ @endphp
-@endwhile
-
-@foreach ($frutas as $fruta)
-    {{ $fruta }} <br>
-@endforeach
+@component('components.sidebar')
+@slot('paragrafo')
+    Texto qualquer vindo so slot
+@endslot
+@endcomponent
 
 @endsection
